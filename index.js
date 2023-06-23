34cbc0bca4772a1814580105a5348f31a985604b
Sayan Chattopadhyay <86191746+sayan6823@users.noreply.github.com>
2023-06-23 09:39:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//  Left Image
+var randomNumber1= Math.floor(Math.random() * 6) +1;
+var locLeft='images/dice'+randomNumber1+'.png';
+var leftImg=document.querySelector('.img1');
+leftImg.setAttribute('src',locLeft);
+
+// Right Image
+var randomNumber2= Math.floor(Math.random() * 6) +1;
+var locRight='images/dice'+randomNumber2+'.png';
+var rightImg=document.querySelector('.img2');
+rightImg.setAttribute('src',locRight);
+
+
+// Setting Heading
+var heading=document.querySelector('h1');
+var message;
+if(randomNumber1>randomNumber2)
+    message='Player1 won !!';
+else if (randomNumber1 < randomNumber2)
+    message='Player2 won !!';
+else
+    message='Its a draw !!';
+
+heading.innerHTML=message;
