fc6205a2702b5c2b0860a6b9fb8f28f44cf60ce6
Sayan Chattopadhyay <86191746+sayan6823@users.noreply.github.com>
2023-06-23 09:47:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //  Left Image
 var randomNumber1= Math.floor(Math.random() * 6) +1;
-var locLeft='Images/dice'+randomNumber1+'.png';
+var locLeft='/Images/dice'+randomNumber1+'.png';
 var leftImg=document.querySelector('.img1');
 leftImg.setAttribute('src',locLeft);
 
 // Right Image
 var randomNumber2= Math.floor(Math.random() * 6) +1;
-var locRight='Images/dice'+randomNumber2+'.png';
+var locRight='/Images/dice'+randomNumber2+'.png';
 var rightImg=document.querySelector('.img2');
 rightImg.setAttribute('src',locRight);
 
